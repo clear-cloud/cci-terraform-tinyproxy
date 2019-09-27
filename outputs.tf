4683e45fa4b96fa17876fb9654ec9cffbf3691f9
Chris.Morgan <chris.morgan@clearcloud.co.uk>
2019-09-27 09:57:53 +0000

    IAM role ARN output

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "asg_name" {
   description = "Auto Scaling Group name"
   value       = "${aws_autoscaling_group.proxy.name}"
 }
+
+output "iam_role_arn" {
+  description = "ARN of IAM role"
+  value       = "${aws_iam_policy.proxy_policy.arn}"
+}
